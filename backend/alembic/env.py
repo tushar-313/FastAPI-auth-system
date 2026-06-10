7fa83fc7b604c28a3e8d3c8e280df9e84a572659
Tushar Agarwal <tusharagrawal318@gmail.com>
2026-06-10 12:48:16 +0530

    alembic error fixed

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -1,7 +1,8 @@
 from logging.config import fileConfig
-
+from app.config import settings
 from app.models import Base, User
 from sqlalchemy import engine_from_config
+from sqlalchemy import create_engine
 from sqlalchemy import pool
 
 from alembic import context
@@ -58,9 +59,7 @@ def run_migrations_online() -> None:
     and associate a connection with the context.
 
     """
-    connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {}),
-        prefix="sqlalchemy.",
+    connectable = create_engine(settings.DATABASE_URL,
         poolclass=pool.NullPool,
     )
 
